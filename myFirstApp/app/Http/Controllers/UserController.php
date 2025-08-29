54968c7013eb11c6ec449299c9fa11cd16bec027
Itode fortune <fortune.nwuneke@gmail.com>
2025-08-30 00:50:36 +0100

    feat: Implement user authentication with login, logout, and registration features; update homepage routing and layout

--- myFirstApp/app/Http/Controllers/UserController.php
+++ myFirstApp/app/Http/Controllers/UserController.php
@@ -5,9 +5,19 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
+    public function showCorrectHomepage()
+    {
+
+        if (Auth::check()) {
+            return view('homepage-feed');
+        } else {
+            return view('homepage');
+        }
+    }
     //
     function register(Request $request)
     {
@@ -21,6 +31,31 @@ class UserController extends Controller
             'email' => $incomingFields['email'],
             'password' => bcrypt($incomingFields['password'])
         ]);
-        return 'this is the register page';
+        return redirect('/');
+    }
+
+    function login(Request $request)
+    {
+        $incomingFields = $request->validate([
+            'loginusername' => ['required'],
+            'loginpassword' => ['required']
+        ]);
+        if (Auth::attempt([
+            'username' => $incomingFields['loginusername'],
+            'password' => $incomingFields['loginpassword']
+        ])) {
+            $request->session()->regenerate();
+            return redirect('/')->with('success', 'You are now logged in!');
+        } else {
+            return redirect('/')->with('failure', 'Invalid username/password');
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/')->with('success', 'You have been logged out.');
     }
 }
--- myFirstApp/routes/web.php
+++ myFirstApp/routes/web.php
@@ -4,9 +4,11 @@ use App\Http\Controllers\ExampleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ExampleController::class, 'homepage']);
+Route::get('/', [UserController::class, 'showCorrectHomepage']);
 
 Route::get('/about', [ExampleController::class, 'aboutPage']);
 
 Route::get('/post', [ExampleController::class, 'post']);
 Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/logout', [UserController::class, 'logout']);
